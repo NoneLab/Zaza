4c290f18ef85cbd8c9999140bcd2793edaf92f9a
Nakada Tokumei <nakada_tokumei@protonmail.com>
2026-07-16 18:37:36 +0900

    chroe: update simplefi submodule integration
    - update the simplefi submodule revision
    - migrate hello_world to Runtime::InitializeRuntime
    - ignore generated compile_commands.json

--- apps/hello_world/main.cc
+++ apps/hello_world/main.cc
@@ -7,7 +7,7 @@
 EFI_STATUS EFIAPI
 EfiMain(EFI_HANDLE ImageHandle, EfiSystemTable* SystemTable)
 {
-    InitializeRuntime(ImageHandle, SystemTable);
+    Runtime::InitializeRuntime(ImageHandle, SystemTable);
     printf("Hello, newlib on UEFI\r\n");
     return EFI_SUCCESS;
 }
